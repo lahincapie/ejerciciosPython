0abf552eb66cc57e4b11c04343d10483964a196d
Alejandra Hincapie <alejandra612@hotmail.com>
2020-04-26 15:25:06 -0500

    Primer ejercicio basico de hola mundo para python 3

---
+++ holaMundoP3.py
@@ -0,0 +1,11 @@
+# Este es un ejemplo sencillo del funcionamiento de un programa en Python-
+# se usa la palabra print como instruccion propia de Python para mostrar en la consola
+# un mensaje. 
+# Dentro de python print hace referencia a una sentencia y lo que se encuantra
+# dentro de las comillas se conoce como cadena o cadena de texto y se identifica 
+# porque precisamente siempre va entre comillas.
+# En pyton 2.7.x print se comparta como una palabra clave y no es necesario usar loas parentesis,
+# sin embargo en pytthon 3 print es una funcion por lo que es nacesario usar los parentesis
+# El resultado que se obtiene al ejecutar es: Hola Mundo 
+
+print ('Hola Mundo')

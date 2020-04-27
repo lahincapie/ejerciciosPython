f6dd083a8ce6a08b7f4fb8e29a240896626a5d60
Alejandra Hincapie <alejandra612@hotmail.com>
2020-04-26 20:48:05 -0500

    uso de raw_input() e input() para que no se cierre la pantalla

---
+++ inputp3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# raw_input() es utilizado para capturar datos desde el teclado, pero es una solucion
+# algo rustica pero funcional, para evitar que la pantalla de la ejecuuin se cierre
+# antes de ver el resultado
+
+print ("hola mundo")
+
+input()

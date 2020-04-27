201f6880761b77b7feb7361ef53b05f248471253
Alejandra Hincapie <alejandra612@hotmail.com>
2020-04-27 10:17:25 -0500

    retorno de carro con print y como evitarlo

---
+++ retornoCarroP3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Cada instruccion print() se imprime en una linea nueva, si lo que se quiere es 
+# que se imprima en la misma linea se tiene que usar ", end"" " dentro de los parentesis.
+ 
+print ("Hola", end=" ")
+print ("Mundo") 
+

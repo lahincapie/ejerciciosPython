8187d352dd4fa9eef0413d8febf940d02334c1f9
Alejandra Hincapie <alejandra612@hotmail.com>
2020-05-01 17:37:30 -0500

    operadore

---
+++ operadores.py
@@ -0,0 +1,18 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+primer_numero = 10
+segundo_numero = 5
+
+suma = primer_numero + segundo_numero
+resta = primer_numero - segundo_numero
+multiplicacion = primer_numero * segundo_numero
+division = primer_numero / segundo_numero
+potencia = primer_numero ** segundo_numero
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(potencia)
